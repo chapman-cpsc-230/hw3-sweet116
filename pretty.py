95eef9260134090701f164a2f65d31f8cd8b9c6e
M. Andrew Moshier <moshier@chapman.edu>
2016-03-03 21:20:34 -0800

    Added examples to INSTRUCTIONS

--- pretty.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-bob = turtle.Pen()
-
-for side in range(4):
-    bob.forward(50)
-    bob.left(90)
---
+++ turtlestarter.py
@@ -0,0 +1,15 @@
+import turtle
+
+# Ask user for input here.
+
+# Now create a graphics window.
+t = turtle.Pen()
+
+# Put the rest of your code can go here
+
+# Prevent the graphics window from diappearing too
+# quickly to see it.
+stopper = raw_input("Hit <enter> to quit.")
+
+# Now remove the graphics window before exiting.
+turtle.bye()
